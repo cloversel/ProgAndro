c45d9a3221f5df4dd7a8a930eaeb23dba12817ae
Raymond Kurniawan <r.kurniawan@ti.ukdw.ac.id>
2020-04-17 22:33:08 +0700

    TugasMinggu8
    tugas pertemuan minggu ke 8.

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/FirebasePage.java
@@ -0,0 +1,190 @@
+package com.example.helloworld;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class FirebasePage extends AppCompatActivity {
+
+    private EditText noMhs;
+    private EditText namaMhs;
+    private EditText phoneMhs;
+    private Button buttonSimpan;
+    private Button buttonHapus;
+    private Button buttonCari;
+    private FirebaseFirestore firebaseFirestoreDb;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_page);
+
+        noMhs = (EditText) findViewById(R.id.noMhs);
+        namaMhs = (EditText) findViewById(R.id.namaMhs);
+        phoneMhs = (EditText) findViewById(R.id.phoneMhs);
+        buttonSimpan = (Button) findViewById(R.id.simpanButton);
+        buttonHapus = (Button) findViewById(R.id.hapusButton);
+        buttonCari = (Button) findViewById(R.id.cariButton);
+
+        firebaseFirestoreDb = FirebaseFirestore.getInstance();
+
+        buttonSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sanity check
+                if (!noMhs.getText().toString().isEmpty() && !namaMhs.getText().toString().isEmpty()) {
+                    tambahMahasiswa();
+                } else {
+                    Toast.makeText(getApplicationContext(), "No dan Nama Mhs tidak boleh kosong",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        buttonHapus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteDataMahasiswa();
+            }
+        });
+
+        buttonCari.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                getDataMahasiswa();
+            }
+        });
+
+
+    }
+
+    public class Mahasiswa {
+
+        private String nim;
+        private String nama;
+        private String phone;
+
+        public Mahasiswa(String nim, String nama, String phone) {
+            this.nim = nim;
+            this.nama = nama;
+            this.phone = phone;
+        }
+
+        public String getNama() {
+            return nama;
+        }
+
+        public void setNama(String nama) {
+            this.nama = nama;
+        }
+
+        public String getNim() {
+            return nim;
+        }
+
+        public void setNim(String nim) {
+            this.nim = nim;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+    }
+
+    private void tambahMahasiswa() {
+
+        Mahasiswa mhs = new Mahasiswa(noMhs.getText().toString(),
+                namaMhs.getText().toString(),
+                phoneMhs.getText().toString());
+
+
+        firebaseFirestoreDb.collection("DaftarMhs").document().set(mhs)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(), "Mahasiswa berhasil didaftarkan",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "ERROR" + e.toString(),
+                                Toast.LENGTH_SHORT).show();
+                        Log.d("TAG", e.toString());
+                    }
+                });
+    }
+
+    private void getDataMahasiswa() {
+        DocumentReference docRef = firebaseFirestoreDb.collection("DaftarMhs").document("mhs1");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Mahasiswa mhs = document.toObject(Mahasiswa.class);
+                        noMhs.setText(mhs.getNim());
+                        namaMhs.setText(mhs.getNama());
+                        phoneMhs.setText(mhs.getPhone());
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Document tidak ditemukan",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Document error : " + task.getException(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void deleteDataMahasiswa() {
+        firebaseFirestoreDb.collection("DaftarMhs").document("mhs1")
+                .delete()
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        noMhs.setText("");
+                        namaMhs.setText("");
+                        phoneMhs.setText("");
+                        Toast.makeText(getApplicationContext(), "Mahasiswa berhasil dihapus",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error deleting document: " + e.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+
+
+
+}
