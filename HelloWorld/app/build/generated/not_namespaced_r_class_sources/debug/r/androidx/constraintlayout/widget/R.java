fa25dda672595148db41a4129acc3392dd473d7a
Raymond Kurniawan <r.kurniawan@ti.ukdw.ac.id>
2020-04-24 23:08:42 +0700

    Pertemuan 9 - Komponen Perangkat Keras

--- HelloWorld/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ HelloWorld/app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080025;
-        public static final int end = 0x7f08004c;
-        public static final int gone = 0x7f08005b;
-        public static final int invisible = 0x7f080067;
-        public static final int left = 0x7f08006c;
-        public static final int packed = 0x7f080082;
-        public static final int parent = 0x7f080084;
-        public static final int percent = 0x7f080088;
-        public static final int right = 0x7f08008d;
-        public static final int spread = 0x7f0800b1;
-        public static final int spread_inside = 0x7f0800b2;
-        public static final int start = 0x7f0800b7;
-        public static final int top = 0x7f0800d2;
-        public static final int wrap = 0x7f0800e3;
+        public static final int end = 0x7f08004d;
+        public static final int gone = 0x7f08005c;
+        public static final int invisible = 0x7f080068;
+        public static final int left = 0x7f08006d;
+        public static final int packed = 0x7f080084;
+        public static final int parent = 0x7f080086;
+        public static final int percent = 0x7f08008a;
+        public static final int right = 0x7f08008f;
+        public static final int spread = 0x7f0800b3;
+        public static final int spread_inside = 0x7f0800b4;
+        public static final int start = 0x7f0800b9;
+        public static final int top = 0x7f0800d4;
+        public static final int wrap = 0x7f0800e5;
     }
     public static final class styleable {
         private styleable() {}
